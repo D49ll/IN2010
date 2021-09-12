5ecfce294c4af04b1e39063c0692e85e77b70b70
Daniel Stangeland <daniel.stangeland@outlook.com>
2021-09-12 19:06:59 +0200

    removed odd/even cases

--- assignments/assignment_1/task4_binary-trees/balancearray.py
+++ assignments/assignment_1/task4_binary-trees/balancearray.py
@@ -1,4 +1,4 @@
-A = [x for x in range(10000)]
+A = [x for x in range(10)]
 
 balanced = []
 
@@ -6,36 +6,39 @@ def print_binary_tree(A,balanced):
     if len(A) == 2:
         balanced.append(A[-1])
         balanced.append(A[-2])
-        print(A[-1])
-        print(A[-2])
+        #print(A[-1])
+        #print(A[-2])
 
     elif len(A) == 1:
         balanced.append(A[-1])
-        print(A[-1])
+        #print(A[-1])
     
-    elif len(A)%2 == 1:
+    #elif len(A)%2 == 1:
+    else:
         parent_index = len(A) // 2
         balanced.append(A[parent_index])
-        print(A[parent_index])
+        #print(A[parent_index])
         right_child_array = A[parent_index+1:]
         left_child_array = A[:parent_index]
+        
         print_binary_tree(right_child_array, balanced)
         print_binary_tree(left_child_array, balanced)
-
+    '''
     else:
         parent_index = (len(A)//2) - 1
         balanced.append(A[parent_index])
-        print(A[parent_index])
+        #print(A[parent_index])
 
         right_child_array = A[parent_index+1:]
         left_child_array = A[:parent_index]
         print_binary_tree(right_child_array, balanced)
         print_binary_tree(left_child_array, balanced)
+    '''
 
     return balanced
 
 test = print_binary_tree(A, balanced)
-#print(test)
+print(test)
 
 #string_ints = [str(int) for int in test]
 
